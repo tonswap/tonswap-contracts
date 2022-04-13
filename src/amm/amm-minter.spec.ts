1ebd559e168ddd135865e54fe1665a6c7688bde3
doron aviguy <doronaviguy@gmail.com>
2022-04-13 17:20:23 +0300

    transfer flow , works

--- src/amm/amm-minter.spec.ts
+++ src/amm/amm-minter.spec.ts
@@ -3,87 +3,137 @@ import {
     Address, Cell, CellMessage, CommonMessageInfo, InternalMessage,
 } from "ton";
 import BN from "bn.js";
-import { JettonMinter } from "../jetton/jetton-minter.debug";
-import { parseActionsList, SendMsgOutAction, parseJettonTransfer } from "../utils";
+import { JettonMinter } from "../jetton/jetton-minter";
+import { parseActionsList, SendMsgOutAction, parseJettonTransfer, sliceToAddress267 } from "../utils";
 import { SmartContract } from "ton-contract-executor";
-import { JettonWallet } from "./amm-wallet.debug";
+import { JettonWallet } from "../jetton/jetton-wallet";
 
 
 const contractAddress = Address.parse('EQD4FPq-PRDieyQKkizFTRtSDyucUIqrj0v_zXJmqaDp6_0t')
-const alice = Address.parseFriendly('kQCLjyIQ9bF5t9h3oczEX3hPVK4tpW2Dqby0eHOH1y5_Nk1x').address;
+const minterAddress = Address.parse('EQD4FPq-PRDieyQKkizFTRtSDyucUIqrj0v_zXJmqaDp6_0t')
+const alice = Address.parseFriendly('EQCLjyIQ9bF5t9h3oczEX3hPVK4tpW2Dqby0eHOH1y5_Nvb7').address;
 const bob = Address.parseFriendly('EQCbPJVt83Noxmg8Qw-Ut8HsZ1lz7lhp4k0v9mBX2BJewhpe').address;
+
+const aliceSubwallet = Address.parseFriendly('EQCLjyIQ9bF5t9h3oczEX3hPVK4tpW2Dqby0eHOH1y5_Nvb7').address;
 
 
 const TRC20_TRANSFER = 0xf8a7ea5;
 const SWAP_OUT_SUB_OP = 8;
-
-
-// var initData = {
-//     name: 'LP Token',
-//     symbol: 'LP',
-//     decimals: new BN(9),
-//     totalSupply: toDecimals(0),
-//     tokenReserves: toDecimals(0),
-//     tonReserves: toDecimals(0),
-//     tokenAddress: SUSHI_TOKEN_V2,
-//     tokenAdmin: TOKEN_ADMIN,
-//     tokenAllocPoints: new BN(500),
-//     protocolAdmin: PROTOCOL_ADMIN,
-//     protocolAllocPoints: new BN(0),
-// } as DexConfig;
+const OP_ADD_LIQUIDITY = 22;
 
 
 describe('Jetton Minter ', () => {
     
+
+    
     it("mint USDC", async () => {
 
-        const masterUSDC = await JettonMinter.create(new BN(0), alice, "https://ipfs.io/ipfs/dasadas");
-        const mintResponse = await masterUSDC.mint(alice, bob, new BN(1500));
-        
-        const mintMessage = mintResponse.actions[0] as SendMsgOutAction;
-        console.log(mintMessage);
-        
-        //send the transfer message to the contract
-        let initMsg = new CommonMessageInfo( { body: new CellMessage(mintMessage.message?.body) });
-        const msg = new InternalMessage({
-            to: bob,
-            from: contractAddress,
-            value: new BN(1000000000), // 0.1 TON
-            bounce: false,
-            body: initMsg
-        })
-        // Deploy USDC Sub wallet based on the output action from the mint result, 
-        // so we take the output message and initiate a contract based on the code data and init state and save reference to it
-        let usdcSubWallet = await JettonWallet.createFromMessage(mintMessage.message?.init?.code as Cell, mintMessage.message?.init?.data as Cell, msg);
+        const {
+            masterUSDC,
+            aliceUSDC
+        } = await initEnvironment();
     
-        const jettonWalletResponse = await usdcSubWallet.getData();
+        const jettonWalletResponse = await aliceUSDC.getData();
         console.log(`jettonWalletResponse  (after send) balance:${jettonWalletResponse.balance.toString()}`);
 
         ///////// mint again
-        const mintResponse2 = await masterUSDC.mint(alice, bob, new BN(2505));
+        const mintResponse2 = await masterUSDC.mint(alice, alice, new BN(2505));
         const mintMessage2 = mintResponse2.actions[0] as SendMsgOutAction;
-        let mintMessageInfo2 = new CommonMessageInfo( { body: new CellMessage(mintMessage2.message?.body) });
-
-        const mintMessageRAW = new InternalMessage({
-            to: bob,
-            from: contractAddress,
-            value: new BN(1000000000), // 0.1 TON
-            bounce: false,
-            body: mintMessageInfo2
-        })
+        const mintMessageRAW = actionToInternalMessage(bob, contractAddress, mintMessage2.message?.body);
         
-        const mintMessageResponse2 = await usdcSubWallet.sendInternalMessage(mintMessageRAW);
+        const mintMessageResponse2 = await aliceUSDC.sendInternalMessage(mintMessageRAW);
         expect(mintMessageResponse2.exit_code).toBe(0);
         
-        const data2 = await usdcSubWallet.getData();
+        const data2 = await aliceUSDC.getData();
         console.log(`jettonWalletResponse2  (after send #2) balance:${ data2.balance.toString()}`);
     })
+
+    it("alice sends bob USDC", async () => {
+        const {
+            masterUSDC,
+            aliceUSDC
+        } = await initEnvironment();
+
+        let aliceData = await aliceUSDC.getData();
+        console.log(`alice owner: ${sliceToAddress267(aliceData.owner).toFriendly()}`);
+        
+        
+
+        const transferResponse = await aliceUSDC.transfer(alice, bob, new BN(502), bob);
+        console.log(transferResponse);
+        
+        const transferMessage = transferResponse.actions[0] as SendMsgOutAction;
+        
+        const msg = actionToInternalMessage(bob, contractAddress, transferMessage.message?.body);
+        const bobUSDC = await JettonWallet.createFromMessage(
+            transferMessage.message?.init?.code as Cell,
+            transferMessage.message?.init?.data as Cell,
+            msg);
+
+        const bobUsdcData = await bobUSDC.getData();
+        console.log(`bobUsdcData after transfer balance:${ bobUsdcData.balance.toString()}  owner: ${ sliceToAddress267(bobUsdcData.owner).toFriendly()}`);
+        
+    })
+
+    it.only("alice adds Liquidity", async () => {
+        const {
+            masterUSDC,
+            aliceUSDC
+        } = await initEnvironment();
+
+        let aliceData = await aliceUSDC.getData();
+        console.log(`alice owner: ${sliceToAddress267(aliceData.owner).toFriendly()}`);
+        
+        
+        let extra_data = new Cell();
+        extra_data.bits.writeUint(OP_ADD_LIQUIDITY ,32);
+        extra_data.bits.writeUint(new BN(5) ,32);  // slippage
+
+        const transferResponse = await aliceUSDC.transfer(alice, bob, new BN(502),  bob, undefined, new BN(10), extra_data);
+        console.log(transferResponse);
+        
+        const transferMessage = transferResponse.actions[0] as SendMsgOutAction;
+        
+        const msg = actionToInternalMessage(bob, contractAddress, transferMessage.message?.body);
+        const bobUSDC = await JettonWallet.createFromMessage(
+            transferMessage.message?.init?.code as Cell,
+            transferMessage.message?.init?.data as Cell,
+            msg);
+
+        const bobUsdcData = await bobUSDC.getData();
+        console.log(`bobUsdcData after transfer balance:${ bobUsdcData.balance.toString()}  owner: ${ sliceToAddress267(bobUsdcData.owner).toFriendly()}`);
+        
+    })
+
 
 });
 
 
+async function initEnvironment() {
+    const masterUSDC = await JettonMinter.create(new BN(0), alice, "https://ipfs.io/ipfs/dasadas");
+    const mintResponse = await masterUSDC.mint(alice, alice, new BN(1500));
+    const mintMessage = mintResponse.actions[0] as SendMsgOutAction;
+    //send the transfer message to the contract
+    const initTransferMessage = actionToInternalMessage(bob, aliceSubwallet, mintMessage.message?.body);
+    // Deploy USDC Sub wallet based on the output action from the mint result, 
+    // so we take the output message and initiate a contract based on the code data and init state and save reference to it
+    let aliceUSDC = await JettonWallet.createFromMessage(mintMessage.message?.init?.code as Cell, mintMessage.message?.init?.data as Cell, initTransferMessage);
 
-function mintJetton(amount: BN) {
+    return {
+        masterUSDC,
+        aliceUSDC
+    }
+} 
 
+
+
+function actionToInternalMessage(to: Address, from:Address, messageBody: Cell, messageValue = new BN(1000000000), bounce = false) {
+    let msg = new CommonMessageInfo( { body: new CellMessage(messageBody) });
+    return new InternalMessage({
+        to,
+        from,
+        value: messageValue,
+        bounce,
+        body: msg
+    })
 }
-
