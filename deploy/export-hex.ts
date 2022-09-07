995c0fa67b73bf39a765372aeca0e7656650bb62
doron aviguy <doronaviguy@gmail.com>
2022-09-07 08:12:00 +0300

    naming

--- deploy/export-hex.ts
+++ deploy/export-hex.ts
@@ -1,13 +1,13 @@
 import { compileFuncToB64 } from "../utils/funcToB64";
 import { writeFileSync } from "fs";
-import {execSync} from "child_process"
+import { execSync } from "child_process";
 
 function main() {
-    let getCommitHash = execSync(`git log --format="%H" -n 1`).toString().trim()
+    let getCommitHash = execSync(`git log --format="%H" -n 1`).toString().trim();
     const ammMinterCodeB64: string = compileFuncToB64(["contracts/amm-minter.fc"]);
-    writeFileSync(`./build/minter.build.json`, `{ "hex":"${ammMinterCodeB64}", "commitHash":"${getCommitHash}" }`);
+    writeFileSync(`./build/amm-minter.build.json`, `{ "hex":"${ammMinterCodeB64}", "commitHash":"${getCommitHash}" }`);
     const ammWalletCode: string = compileFuncToB64(["contracts/amm-wallet.fc"]);
-    writeFileSync(`./build/wallet.build.json`, `{ "hex":"${ammWalletCode}", "commitHash":"${getCommitHash}" }`);
+    writeFileSync(`./build/amm-wallet.build.json`, `{ "hex":"${ammWalletCode}", "commitHash":"${getCommitHash}" }`);
 }
 
 main();
